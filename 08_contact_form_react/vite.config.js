39cda0a2f6f5a56740ac40fc2b62d299ae87e1cf
shun.nagahara <ringorian0626@gmail.com>
2025-10-22 00:55:41 -0700

    fix path problem

--- 08_contact_form_react/vite.config.js
+++ 08_contact_form_react/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/html-css/",
+  base: "/html-css/08_contact_form_react/",
 });
